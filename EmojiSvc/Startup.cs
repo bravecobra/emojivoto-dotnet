8f2be4308970cf439b4fb22022d8efffd0d0f655
bravecobra <391353+bravecobra@users.noreply.github.com>
2021-09-29 14:27:33 +0200

    Added AWS Distro support and improved logging with Seq (#14)
    * Added AWS Distro support and improved logging with Seq
    * Added docker build contexts in docker-compose.override.yml
    * Corrected minor css issues

--- EmojiSvc/Startup.cs
+++ EmojiSvc/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,9 @@ using EmojiSvc.Domain.Impl;
 using EmojiSvc.Persistence;
 using EmojiSvc.Persistence.Impl;
 using EmojiSvc.Services;
+using Microsoft.Extensions.Configuration;
+using OpenTelemetry;
+using OpenTelemetry.Contrib.Extensions.AWSXRay.Trace;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
@@ -16,6 +20,12 @@ namespace EmojiSvc
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,27 +34,50 @@ namespace EmojiSvc
             services.AddSingleton<IEmojiRepo, InMemoryAllEmoji>();
             services.AddTransient<IAllEmoji, AllEmoji>();
             services.AddAutoMapper(typeof(EmojiProfile));
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true); //AWS
             var resourceBuilder = ResourceBuilder.CreateDefault()
                 .AddService(Assembly.GetEntryAssembly()?.GetName().Name)
                 .AddTelemetrySdk();
             services.AddOpenTelemetryTracing(
-                (builder) => builder
-                    .AddAspNetCoreInstrumentation(options =>
+                (builder) =>
+                {
+                    builder
+                        .AddAspNetCoreInstrumentation(options =>
+                        {
+                            options.RecordException = true;
+                            options.EnableGrpcAspNetCoreSupport = true;
+                        })
+                        .AddXRayTraceId()
+                        .AddAWSInstrumentation()
+                        .AddGrpcCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddGrpcClientInstrumentation()
+                        .SetResourceBuilder(resourceBuilder);
+                    var consoleExport = _configuration.GetValue<bool>("CONSOLE_EXPORT");
+                    if (consoleExport)
                     {
-                        options.RecordException = true;
-                        options.EnableGrpcAspNetCoreSupport = true;
-                    })
-                    .AddGrpcCoreInstrumentation()
-                    .AddHttpClientInstrumentation()
-                    .AddGrpcClientInstrumentation()
-                    .AddConsoleExporter()
-                    .AddJaegerExporter(options =>
+                        builder.AddConsoleExporter();
+                    }
+                    var jaegerHost = _configuration.GetValue<string>("JAEGER_HOST");
+                    var jaegerPort = _configuration.GetValue<int>("JAEGER_PORT");
+                    if (!string.IsNullOrEmpty(jaegerHost))
                     {
-                        options.AgentHost = "jaeger";
-                        options.AgentPort = 6831;
-                    })
-                    .SetResourceBuilder(resourceBuilder)
-            );
+                        builder.AddJaegerExporter(options =>
+                        {
+                            options.AgentHost = jaegerHost;
+                            options.AgentPort = jaegerPort;
+                        });
+                    }
+                    var otelUri = _configuration["AWS_OTEL_URI"];
+                    if (!string.IsNullOrEmpty(otelUri))
+                    {
+                        builder.AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri(otelUri);
+                        });
+                    }
+                });
+            Sdk.SetDefaultTextMapPropagator(new AWSXRayPropagator());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- EmojiVoteBot/Program.cs
+++ EmojiVoteBot/Program.cs
@@ -6,9 +6,15 @@ using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using OpenTelemetry;
+using OpenTelemetry.Contrib.Extensions.AWSXRay.Trace;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace EmojiVoteBot
 {
@@ -24,29 +30,68 @@ namespace EmojiVoteBot
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(builder => builder.AddOpenTelemetry())
+                //.ConfigureLogging(builder => builder.AddOpenTelemetry())
+                .UseSerilog((context, loggerConfiguration) =>
+                    loggerConfiguration
+                        .ReadFrom.Configuration(context.Configuration)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithAssemblyName()
+                        .Enrich.WithAssemblyVersion()
+                        .Enrich.WithAssemblyInformationalVersion()
+                        .Enrich.WithEnvironment(context.HostingEnvironment.EnvironmentName)
+                        .Enrich.WithProcessId()
+                        .Enrich.WithProcessName()
+                        .Enrich.WithThreadId()
+                        .Enrich.WithThreadName()
+                        .Enrich.WithSpan()
+                        .Enrich.WithExceptionDetails()
+                        .WriteTo.Console(new RenderedCompactJsonFormatter())
+                        .WriteTo.Seq(context.Configuration["SEQ_URI"]))
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHttpClient();
                     services.AddTransient<IEmojiVoteService, EmojiVoteRestService>();
                     services.AddHostedService<VotingBot>();
+                    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
                     var resourceBuilder = ResourceBuilder.CreateDefault()
                         .AddService(Assembly.GetEntryAssembly()?.GetName().Name)
                         .AddTelemetrySdk();
                     services.AddOpenTelemetryTracing(
-                        (builder) => builder
-                            .SetSampler(new AlwaysOnSampler())
-                            .AddSource(nameof(VotingBot))
-                            .AddHttpClientInstrumentation()
-                            .AddGrpcClientInstrumentation()
-                            .AddConsoleExporter()
-                            .AddJaegerExporter(options =>
+                        (builder) =>
+                        {
+                            builder
+                                .SetSampler(new AlwaysOnSampler())
+                                .AddSource(nameof(VotingBot))
+                                .AddXRayTraceId()
+                                .AddAWSInstrumentation()
+                                .AddHttpClientInstrumentation()
+                                .AddGrpcClientInstrumentation()
+                                .SetResourceBuilder(resourceBuilder);
+                            var consoleExport = hostContext.Configuration.GetValue<bool>("CONSOLE_EXPORT");
+                            if (consoleExport)
                             {
-                                options.AgentHost = "jaeger";
-                                options.AgentPort = 6831;
-                            })
-                            .SetResourceBuilder(resourceBuilder)
-                    );
+                                builder.AddConsoleExporter();
+                            }
+                            var jaegerHost = hostContext.Configuration.GetValue<string>("JAEGER_HOST");
+                            var jaegerPort = hostContext.Configuration.GetValue<int>("JAEGER_PORT");
+                            if (!string.IsNullOrEmpty(jaegerHost))
+                            {
+                                builder.AddJaegerExporter(options =>
+                                {
+                                    options.AgentHost = jaegerHost;
+                                    options.AgentPort = jaegerPort;
+                                });
+                            }
+                            var otelUri = hostContext.Configuration["AWS_OTEL_URI"];
+                            if (!string.IsNullOrEmpty(otelUri))
+                            {
+                                builder.AddOtlpExporter(options =>
+                                {
+                                    options.Endpoint = new Uri(otelUri);
+                                });
+                            }
+                        });
+                    Sdk.SetDefaultTextMapPropagator(new AWSXRayPropagator());
                 });
     }
 }
